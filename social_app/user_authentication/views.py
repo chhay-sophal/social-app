0e84d5ff168aa88d7a4102b2afdbeebd19c2cc4a
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2023-10-07 14:21:44 +0700

    Add RegisterView, CustomLoginView, ProfileView, and ProfileUpdateView to views.py

--- social_app/user_authentication/views.py
+++ social_app/user_authentication/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import authenticate, login
+from django.views.generic import TemplateView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from .forms import RegistrationForm, LoginForm, ProfileForm, ProfileUpdateForm
+from .models import Profile
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    form_class = RegistrationForm
+    template_name = 'user_authentication/register.html'
+    success_url = reverse_lazy('login')
+
+
+class CustomLoginView(LoginView):
+    template_name = 'user_authentication/login.html'
+    authentication_form = LoginForm
+
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'user_authentication/profile.html'
+
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    form_class = ProfileUpdateForm
+    template_name = 'user_authentication/profile_update.html'
+    success_url = reverse_lazy('profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user.profile
