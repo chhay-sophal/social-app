c72402e1b8fa6322ecca3ca5859b5481c06f720f
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2023-10-24 11:48:48 +0700

    only display the posts from the current user and the following users

--- social_app/apps/posts/views.py
+++ social_app/apps/posts/views.py
@@ -3,8 +3,10 @@ from django.urls import reverse
 from django.views import View
 from django.views.generic import ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from .models import Post
 from apps.user_authentication.models import Profile
+from apps.follows.models import Follow
 from .forms import *
 
 
@@ -15,8 +17,15 @@ class PostListView(LoginRequiredMixin, ListView):
     paginate_by = 10  # Number of posts to display per page
 
     def get_queryset(self):
-        # Get all posts ordered by created_at in descending order
-        posts = Post.objects.order_by('-created_at')
+        # Get the current user
+        user = self.request.user
+
+        # Get the authors that the current user is following
+        following_authors = Follow.objects.filter(from_user=user).values_list('to_user', flat=True)
+
+        # Retrieve posts from the following authors and the current user
+        posts = Post.objects.filter(Q(author__in=following_authors) | Q(author=user)).order_by('-created_at')
+
         return posts
 
 
