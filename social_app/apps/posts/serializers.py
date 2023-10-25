bbc308f5664659c1b40461cc36a364f537522059
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2023-10-25 15:45:48 +0700

    add class ProfileSerializer to get the get the picture from the profile

--- social_app/apps/posts/serializers.py
+++ social_app/apps/posts/serializers.py
@@ -1,11 +1,26 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Comment
+from apps.user_authentication.models import Profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    profile_picture = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Profile
+        fields = ['profile_picture']
+
+    def get_profile_picture(self, obj):
+        return obj.get_profile_picture_url()
+    
 
 class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
     class Meta:
         model = User
-        fields = ['username']
+        fields = ['username', 'profile']
+
 
 class CommentSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
